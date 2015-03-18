46437b5f790bce2f38747c1b77bc916728b41b9d
Charles Marchand <charles.marc13@gmail.com>
2015-03-18 19:31:19 -0400

    Update High

--- MARC120695_Lab4/Program.cs
+++ MARC120695_Lab4/Program.cs
@@ -10,6 +10,68 @@ namespace Lab4
     {
         //Le programe ne marche pas à cause de GetTaille.  Suggestion : Modelisé, Méthodes dans Program.cs, ne pas vérifier taille dans 
 
+        public Banane creerBanane()
+        {
+            Random rnd = new Random();
+            sbyte variete;//variete 1-2-3
+            DateTime dateCueillette;
+            sbyte murete;//0-100
+            int range;
+
+            Banane banane = new Banane();
+
+            //Variete aléatoire
+            variete = sbyte.Parse(Convert.ToString(rnd.Next(1, 4)));
+
+            //Date Aléatoire
+            dateCueillette = new DateTime(1995, 1, 1);
+            range = (DateTime.Today - dateCueillette).Days;
+            dateCueillette = dateCueillette.AddDays(rnd.Next(range));
+
+            //Murete Aléatoire
+            murete = sbyte.Parse(Convert.ToString(rnd.Next(1, 101)));
+
+            banane.banane(variete, dateCueillette);
+
+            banane.murir(murete);
+
+            return (banane);
+        }
+
+        public Pomme creerPomme()
+        {
+            Random rnd = new Random();
+            sbyte variete;//variete 1-2-3
+            DateTime dateCueillette;
+            sbyte murete;//0-100
+            int range;
+            bool contientUnVer = false;
+
+            Pomme pomme = new Pomme();
+
+            //Variete aléatoire
+            variete = sbyte.Parse(Convert.ToString(rnd.Next(1, 4)));
+
+            //Date Aléatoire
+            dateCueillette = new DateTime(1995, 1, 1);
+            range = (DateTime.Today - dateCueillette).Days;
+            dateCueillette = dateCueillette.AddDays(rnd.Next(range));
+
+            //Murete Aléatoire
+            murete = sbyte.Parse(Convert.ToString(rnd.Next(1, 101)));
+
+            //contientUnVer Aléatoire
+            range = rnd.Next(1, 3);
+
+            if (range == 1)
+
+
+
+
+            return (pomme);
+
+            
+        }
 
 
         static void Main(string[] args)
@@ -41,13 +103,13 @@ namespace Lab4
                 #region banane
                 Banane banane = new Banane();
 
-                variete = sbyte.Parse(Convert.ToString(rnd.Next(0,4)));
+                variete = sbyte.Parse(Convert.ToString(rnd.Next(1,4)));
                 
                 dateCueillette = new DateTime(1995, 1, 1);
                 range = (DateTime.Today - dateCueillette).Days; 
                 dateCueillette = dateCueillette.AddDays(rnd.Next(range));
 
-                murete = sbyte.Parse(Convert.ToString(rnd.Next(0,101)));
+                murete = sbyte.Parse(Convert.ToString(rnd.Next(1,101)));
 
                 banane.banane(variete,dateCueillette);
 
